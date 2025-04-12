2d12a34e3520c3c28b0656b651d331e3040a574a
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2025-04-12 13:41:18 -0400

    updated

--- BankBalanceApp.java
+++ BankBalanceApp.java
@@ -2,115 +2,94 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class BankBalanceApp extends JFrame{
 
-    // declare private fields
+public class BankBalanceApp extends JFrame {
+
     private JTextField deposit;
     private JTextField withdraw;
     private JLabel balanceLabel;
-    // everyone gets a 100$ signing bonus for their new accounts, yay!
     private double accountBalance = 100.0;
 
+    /**
+     * main app window, lays out components and
+     * sets up event listeners for buttons clicked
+     */
     public BankBalanceApp() {
         super("Banking Application");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
         setSize(600, 400);
-        // center the window 
         setLocationRelativeTo(null);
-    
-        // make the main panel
-        JPanel mainPanel = new JPanel();
-        // two by two grid
-        mainPanel.setLayout(new GridLayout(2, 2, 5, 5));
-    
-        // create components for the main panel
-    
+
+        JPanel mainPanel = new JPanel(new GridLayout(2, 2, 5, 5));
+
         JLabel depositLabel = new JLabel("Amount for deposit:");
         deposit = new JTextField();
-    
+
         JLabel withdrawLabel = new JLabel("Withdraw Amount:");
         withdraw = new JTextField();
-    
-        // add to mainPanel
+
         mainPanel.add(depositLabel);
         mainPanel.add(deposit);
         mainPanel.add(withdrawLabel);
         mainPanel.add(withdraw);
-    
-        // create a second panel for the buttons
+
         JPanel buttonPanel = new JPanel();
-        
+
         JButton depositButton = new JButton("Deposit");
         JButton withdrawButton = new JButton("Withdraw");
-        
 
         balanceLabel = new JLabel(String.format("Current Balance: %.2f", accountBalance));
-    
-        // ad buttons and label to the buttonPanel
+
         buttonPanel.add(depositButton);
         buttonPanel.add(withdrawButton);
         buttonPanel.add(balanceLabel);
-        
-        // create button listener for deposit button
+
         depositButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent act){
-                try{
+            public void actionPerformed(ActionEvent act) {
+                try {
                     double depositAmount = Double.parseDouble(deposit.getText());
-                    // make sure deposit is greater than 0
-                    if( depositAmount <= 0){
+                    if (depositAmount <= 0) {
                         JOptionPane.showMessageDialog(BankBalanceApp.this, "Cannot deposit 0 or negative amount");
-                        balanceLabel.setText(String.format("Current Balance: %.2f", accountBalance));
-                    }else{
-                        // add deposit to account balance
+                    } else {
                         accountBalance += depositAmount;
                         deposit.setText("");
-                        balanceLabel.setText(String.format("Current Balance: %.2f", accountBalance));
                     }
-
-                }catch(NumberFormatException e){
+                    balanceLabel.setText(String.format("Current Balance: %.2f", accountBalance));
+                } catch (NumberFormatException e) {
                     JOptionPane.showMessageDialog(BankBalanceApp.this, e);
                 }
             }
         });
 
-        // create withdraw button listener
         withdrawButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent act){
-                double withdrawAmount = Double.parseDouble(withdraw.getText());
+            public void actionPerformed(ActionEvent act) {
                 try {
-                    if(withdrawAmount <=0 ){
+                    double withdrawAmount = Double.parseDouble(withdraw.getText());
+                    if (withdrawAmount <= 0) {
                         JOptionPane.showMessageDialog(BankBalanceApp.this, "Cannot withdraw 0 or negative");
-                        
-                    }else if (withdrawAmount > accountBalance){
+                    } else if (withdrawAmount > accountBalance) {
                         JOptionPane.showMessageDialog(BankBalanceApp.this, "Insufficient Funds: Cannot withdraw more than you have");
-                        
-                    }else {
+                    } else {
                         accountBalance -= withdrawAmount;
                         withdraw.setText("");
-                        balanceLabel.setText(String.format("Current Balance: %.2f", accountBalance));
                     }
-
                     balanceLabel.setText(String.format("Current Balance: %.2f", accountBalance));
-                }catch(NumberFormatException e){
+                } catch (NumberFormatException e) {
                     JOptionPane.showMessageDialog(BankBalanceApp.this, e);
                 }
             }
         });
 
-        // add the panels to the frame
         add(mainPanel, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
-    
-        // MAKE IT SHOW UP ON THE SCREEN!
         setVisible(true);
     }
-    
-    public static void main(String[] args){
+
+
+    public static void main(String[] args) {
         new BankBalanceApp();
     }
 }
